4a5af2e0fed518ca655012bbc1771eef9220bc88
=binh nguyen <=binhnguyen230494@gmail.com>
2020-08-27 14:19:27 +0700

    Using fluent validation

--- CoreMVCViewModels/System/Users/LoginRequestValidator.cs
+++ CoreMVCViewModels/System/Users/LoginRequestValidator.cs
@@ -4,13 +4,13 @@ using System.Text;
 using FluentValidation;
 namespace CoreMVCViewModels.System.Users
 {
-    public class LoginRequestValidator
+    public class LoginRequestValidator: AbstractValidator<LoginRequest>
     {
         public LoginRequestValidator()
         {
-            /*RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
             RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required")
-                .MinimumLength(6).WithMessage("Password is at least 6 characters");*/
+                .MinimumLength(6).WithMessage("Password is at least 6 characters");
         }
     }
 }
